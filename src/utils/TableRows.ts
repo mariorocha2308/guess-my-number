1a024d7613003b11a625861a3429510dedeffb01
Mario Rocha <61888873+mariorocha2308@users.noreply.github.com>
2023-11-02 21:45:43 -0600

    Update TableRows.ts

--- src/utils/TableRows.ts
+++ src/utils/TableRows.ts
@@ -6,5 +6,11 @@ export const rows = [
   { cell: 4, attemp: [], correctNumbers: 0, correctPositions: 0 },
   { cell: 5, attemp: [], correctNumbers: 0, correctPositions: 0 },
   { cell: 6, attemp: [], correctNumbers: 0, correctPositions: 0 },
-  { cell: 7, attemp: [], correctNumbers: 0, correctPositions: 0 }
+  { cell: 7, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 8, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 9, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 10, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 11, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 12, attemp: [], correctNumbers: 0, correctPositions: 0 },
+  { cell: 13, attemp: [], correctNumbers: 0, correctPositions: 0 }
 ]
